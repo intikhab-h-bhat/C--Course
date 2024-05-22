0946024896f60643426c369f1250c00fadf04faf
intikhab-h-bhat <intikhabh@gmail.com>
2024-05-22 22:04:25 +0530

    First Name and last name from Full name

--- C#-Code/SwapNumbers.cs
+++ C#-Code/SwapNumbers.cs
@@ -1,32 +1,32 @@
 
-// See https://aka.ms/new-console-template for more information
+// // See https://aka.ms/new-console-template for more information
 
-//program to swap the value of two numbers
+// //program to swap the value of two numbers
 
-Console.WriteLine("Enter the value of num1");
-//taking input from the user and assigning it to num1
-int num1=Convert.ToInt16(Console.ReadLine());
+// Console.WriteLine("Enter the value of num1");
+// //taking input from the user and assigning it to num1
+// int num1=Convert.ToInt16(Console.ReadLine());
 
-Console.WriteLine("Enter the value of num2");
-//taking input from the user and assigning it to num2
-int num2=Convert.ToInt16(Console.ReadLine());
+// Console.WriteLine("Enter the value of num2");
+// //taking input from the user and assigning it to num2
+// int num2=Convert.ToInt16(Console.ReadLine());
 
-//declare a temp variable
-int temp;
-//swaping the number num1 value to temp variable
-temp=num1;
+// //declare a temp variable
+// int temp;
+// //swaping the number num1 value to temp variable
+// temp=num1;
 
-// num2 value in num1
-num1=num2;
+// // num2 value in num1
+// num1=num2;
 
-// temp value in num2
-num2=temp;
+// // temp value in num2
+// num2=temp;
 
-Console.WriteLine("After swaping the value of num1 and num2:");
+// Console.WriteLine("After swaping the value of num1 and num2:");
 
-Console.WriteLine("num1 is:");
-Console.WriteLine(num1);
+// Console.WriteLine("num1 is:");
+// Console.WriteLine(num1);
 
-Console.WriteLine("num2 is:");
-Console.WriteLine(num2);
+// Console.WriteLine("num2 is:");
+// Console.WriteLine(num2);
 
