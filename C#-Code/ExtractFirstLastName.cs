239160f68098edbf60ea465283b3931ab44b3a32
intikhab-h-bhat <intikhabh@gmail.com>
2024-05-23 06:45:54 +0530

    User profile

--- C#-Code/ExtractFirstLastName.cs
+++ C#-Code/ExtractFirstLastName.cs
@@ -1,9 +1,9 @@
 // ### 1. Extracting First and Last Names
 // Write a C# program that prompts the user for their full name and then extracts their first and last names.
 
-Console.WriteLine("Enter Your Full Name");
-string fullName = Console.ReadLine();
-string firstName = fullName.Substring(0, fullName.IndexOf(' '));
-string lastName = fullName.Substring(fullName.IndexOf(' '));
-Console.WriteLine($"First Name: {firstName}");
-Console.WriteLine($"Last Name: {lastName}");
+// Console.WriteLine("Enter Your Full Name");
+// string fullName = Console.ReadLine();
+// string firstName = fullName.Substring(0, fullName.IndexOf(' '));
+// string lastName = fullName.Substring(fullName.IndexOf(' '));
+// Console.WriteLine($"First Name: {firstName}");
+// Console.WriteLine($"Last Name: {lastName}");
