0da822f0ea95a803ccd903c6bf5833b7ab8bf365
intikhab-h-bhat <intikhabh@gmail.com>
2024-05-23 09:52:50 +0530

    Text Analyzer

---
+++ C#-Code/TextAnalyzer.cs
@@ -0,0 +1,15 @@
+// ### 4: Advanced Text Analyzer
+//  Write a C# program that acts as a basic text analyzer. 
+//  - It should Accept user input. Allow the user to enter a string (paragraph or text).
+//  - Analyze and report statistics:
+//     - Count the total number of words (consider spaces as word separators).
+//     - Calculate the average word length. [Hint. Average word length = Summation of all letters in each word / Total number of words]
+
+Console.WriteLine("Enter the text here.");
+string strParagraph = Console.ReadLine();
+
+int countWords = strParagraph.Split(' ').Length;
+Console.WriteLine($"Total No of words are : {countWords}");
+
+int avgWordLength = strParagraph.Replace(" ", "").Count()/countWords;
+Console.WriteLine($"Average word length is : {avgWordLength}");
