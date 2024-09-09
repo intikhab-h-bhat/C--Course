9a318551615393232fb921cb07249bb4707b674c
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-09 13:21:25 +0530

    working with strings

--- C#-Code/Program.cs
+++ C#-Code/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+string firstName="   Intikhab";
+string lastName="Bhat         ";
+lastName=lastName.Replace(lastName,"Hussain Bhat");
+Console.WriteLine($"My full name is {firstName.TrimStart()}  {lastName.TrimEnd()}");
--- C#-Code/TextAnalyzer.cs
+++ C#-Code/TextAnalyzer.cs
@@ -5,13 +5,13 @@
 //     - Count the total number of words (consider spaces as word separators).
 //     - Calculate the average word length. [Hint. Average word length = Summation of all letters in each word / Total number of words]
 
-Console.WriteLine("Enter the text here.");
-string strParagraph = Console.ReadLine();
+// Console.WriteLine("Enter the text here.");
+// string strParagraph = Console.ReadLine();
 
-//Total number of words in a paragraph
-int countWords = strParagraph.Split(' ').Length;
-Console.WriteLine($"Total No of words are : {countWords}");
+// //Total number of words in a paragraph
+// int countWords = strParagraph.Split(' ').Length;
+// Console.WriteLine($"Total No of words are : {countWords}");
 
-//Calculating average word Length
-int avgWordLength = strParagraph.Replace(" ", "").Count()/countWords;
-Console.WriteLine($"Average word length is : {avgWordLength}");
+// //Calculating average word Length
+// int avgWordLength = strParagraph.Replace(" ", "").Count()/countWords;
+// Console.WriteLine($"Average word length is : {avgWordLength}");
