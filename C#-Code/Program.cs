84f8ec415c42cef76533d2c29be3b2ada42ebd7d
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-09 14:01:07 +0530

    working with numbers

--- C#-Code/Program.cs
+++ C#-Code/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string firstName="   Intikhab";
-string lastName="Bhat         ";
-lastName=lastName.Replace(lastName,"Hussain Bhat");
-Console.WriteLine($"My full name is {firstName.TrimStart()}  {lastName.TrimEnd()}");
+// string firstName="   Intikhab";
+// string lastName="Bhat         ";
+// lastName=lastName.Replace(lastName,"Hussain Bhat");
+// Console.WriteLine($"My full name is {firstName.TrimStart()}  {lastName.TrimEnd()}");
+decimal a=21.5M;
+decimal b=21.2M;
+decimal c=checked(a+b);
+Console.WriteLine("addition");
+Console.Write(c);
