a68a9f4f4fe3e61ae11a508a07ab0a6c1b676d29
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-10 08:38:16 +0530

    Switch statement

--- C#-Code/Program.cs
+++ C#-Code/Program.cs
@@ -1,14 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome To C#!");
 
-//Console Input output
+//switch statement
 
-Console.Write("Enter your Name: ");
-string name=Console.ReadLine();
-Console.Write("Enter Your age: ");
-string age=Console.ReadLine();
+Console.WriteLine("Enter the day of the week:");
 
-Console.Write("Your name is ");
-Console.Write(name);
-Console.Write(" and your age is ");
-Console.Write(age);
+int day=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(day);
+
+switch (day){
+    case 1:Console.WriteLine("Monday"); 
+        break;
+    case 2:Console.WriteLine("Tuesday"); 
+        break;
+    case 3:Console.WriteLine("Wednesday"); 
+        break;
+    case 4:Console.WriteLine("Thursday"); 
+        break;
+    case 5:Console.WriteLine("Friday"); 
+        break;
+    case 6:Console.WriteLine("Saturday"); 
+        break;        
+    case 7:Console.WriteLine("Sunday"); 
+        break;
+    default:Console.WriteLine("Enter the number between 1 and 7"); 
+        break;
+}
+
+
+
+
