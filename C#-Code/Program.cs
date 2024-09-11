5fb215da1ff430d723441c796b612651029a8ca5
intikhab-h-bhat <intikhabh@gmail.com>
2024-09-11 11:43:31 +0530

    lists dict array

--- C#-Code/Program.cs
+++ C#-Code/Program.cs
@@ -1,63 +1,17 @@
-﻿// int[] numbers =new int[3];
+﻿int num=7;
+int length=5;
+int[] numbers=new int[length];
 
-// for(int i = 0; i < numbers.Length;i++){
-//     Console.WriteLine($"Enter the angle {i +1}");
-//     numbers[i] = Convert.ToInt32(Console.ReadLine());
-// }
+for(int i=1;i<=length; i++) {
 
-// int total=0;
-// // for(int i = 0;i<numbers.Length;i++){
+    numbers[i-1]=i*num;
+}
 
-// //     Console.Write($"{numbers[i]} ");
-// //     total=total+numbers[i];
-    
-// // }
-// Console.WriteLine();
-// foreach(int i in numbers){
-    
-//     Console.Write($"{i} ");
-//     total=total+i;
-// }
-
-
-// Console.WriteLine(total==180?"Valid":"Invalid");
-
-
-int[] numbers= new int[] {1,2,3,7,5,8,9};
-
-// foreach(int num in numbers){
-
-// Console.Write(num);
-// }
-
-// Array.Sort(numbers);
-// Array.Reverse(numbers);
-//Array.Clear(numbers, 5, numbers.Length-5);
-
-// for(int i=0 ;i< numbers.Length;i++)
-// {
-// Console.WriteLine($"{i} ,{numbers[i]}");
-// }
-
-Console.WriteLine("Enter the search number");
-int searchNum=Convert.ToInt32(Console.ReadLine());
-int position=Array.IndexOf(numbers,searchNum);
-int position1=Array.IndexOf(numbers,searchNum,2);
-int position2=Array.IndexOf(numbers,searchNum,2,2);
-Console.WriteLine($"The index of : {position}");
-// int position = -1;
-// for(int i=0;i<numbers.Length;i++){
-
-//     if(numbers[i]==searchNum){
-        
-       
-//         position=i;
-//     }
-    
-// }
-// if(position>-1){
-//      Console.WriteLine($"The index of {searchNum} is : {position}");
-// }
-// else{
-//     Console.WriteLine($"Number not found  {position");
-// }
+for(int i=0;i<length;i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+Console.WriteLine();
+foreach(int i in numbers) {
+    Console.Write($"{i} ");
+}
